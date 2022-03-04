5aab5e94db2f98b4a45d1a4bb590b34cbdc42edf
bigbiggo <100703501+bigbiggo@users.noreply.github.com>
2022-03-04 18:07:25 +0800

    Add files via upload

---
+++ projects/pyqt/qt_init/tmp.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5 import QtWidgets, QtCore
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    widget = QtWidgets.QWidget()
+    widget.resize(320, 240)
+    widget.setWindowTitle("Hello PyQt5")
+    widget.show()
+    sys.exit(app.exec_())
